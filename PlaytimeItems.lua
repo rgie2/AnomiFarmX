70cee51adb2eb50fb220d414db90c69de111bc95
oinkthepig1234 <158383126+rgie2@users.noreply.github.com>
2025-01-18 21:42:58 -0500

    Create PlaytimeItems.lua

---
+++ PlaytimeItems.lua
@@ -0,0 +1,370 @@
+-- Decompiler will be improved soon!
+-- Decompiled with Konstant V2.1, a fast Luau decompiler made in Luau by plusgiant5 (https://discord.gg/wyButjTMhM)
+-- Decompiled on 2025-01-18 18:22:55
+-- Luau version 6, Types version 3
+-- Time taken: 0.003015 seconds
+
+return {
+	Sheriff = {
+		["Sheriff Vest"] = {
+			MinPlaytime = 3;
+			TeamSpecific = true;
+		};
+		["Riot Helmet"] = {
+			MinPlaytime = 5;
+			TeamSpecific = true;
+		};
+		["Sheriff SUV"] = {
+			MinPlaytime = 6;
+			TeamSpecific = true;
+		};
+		["Riot Shotgun"] = {
+			MinPlaytime = 18;
+			TeamSpecific = true;
+		};
+		["Police Transporter"] = {
+			MinPlaytime = 24;
+			TeamSpecific = true;
+		};
+		["Riot PDW"] = {
+			MinPlaytime = 36;
+			TeamSpecific = true;
+		};
+		["Door Ram"] = {
+			MinPlaytime = 48;
+			TeamSpecific = true;
+		};
+	};
+	SWAT = {
+		["Tactical SMG"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Battle Rifle"] = {
+			MinPlaytime = 4;
+			TeamSpecific = true;
+		};
+		["Riot Shotgun"] = {
+			MinPlaytime = 6;
+			TeamSpecific = true;
+		};
+		["Door Ram"] = {
+			MinPlaytime = 6;
+			TeamSpecific = true;
+		};
+		["SWAT SUV"] = {
+			MinPlaytime = 6;
+			TeamSpecific = true;
+		};
+		["SWAT Transporter"] = {
+			MinPlaytime = 8;
+			TeamSpecific = true;
+		};
+		["Combat Shotgun"] = {
+			MinPlaytime = 12;
+			TeamSpecific = true;
+		};
+	};
+	["Cab Driver"] = {
+		Bus = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["School Bus"] = {
+			MinPlaytime = 6;
+			TeamSpecific = false;
+		};
+	};
+	["Safe Seven"] = {
+		["PDW .45"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Bullpup Shotgun"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Riot Helmet"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+	};
+	Military = {
+		["ACH Helmet"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Door Ram"] = {
+			MinPlaytime = 12;
+			TeamSpecific = true;
+		};
+		Humvee = {
+			MinPlaytime = 15;
+			TeamSpecific = true;
+		};
+	};
+	["US Marshal"] = {
+		["Tactical SMG"] = {
+			MinPlaytime = 3;
+			TeamSpecific = true;
+		};
+		["USM SUV"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Door Ram"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+	};
+	["Advanced Gunsmith"] = {
+		["Classic Pistol"] = {
+			MinPlaytime = 2;
+			TeamSpecific = false;
+		};
+		["Classic Revolver"] = {
+			MinPlaytime = 3;
+			TeamSpecific = false;
+		};
+		["Heavy Vest"] = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["Battle Helmet"] = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["Bullpup SMG"] = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["Riot Shotgun"] = {
+			MinPlaytime = 6;
+			TeamSpecific = false;
+		};
+		Carbine = {
+			MinPlaytime = 8;
+			TeamSpecific = false;
+		};
+		["Combat Shotgun"] = {
+			MinPlaytime = 12;
+			TeamSpecific = false;
+		};
+		["Battle Rifle"] = {
+			MinPlaytime = 12;
+			TeamSpecific = false;
+		};
+		["Bullpup Rifle"] = {
+			MinPlaytime = 14;
+			TeamSpecific = false;
+		};
+		Kalashnikov = {
+			MinPlaytime = 18;
+			TeamSpecific = false;
+		};
+		["Heavy Rifle"] = {
+			MinPlaytime = 32;
+			TeamSpecific = false;
+		};
+		["Lever Rifle"] = {
+			MinPlaytime = 30;
+			TeamSpecific = false;
+		};
+		SKS = {
+			MinPlaytime = 48;
+			TeamSpecific = false;
+		};
+	};
+	Gunsmith = {
+		["Combat Pistol"] = {
+			MinPlaytime = 6;
+			TeamSpecific = false;
+		};
+		["Heavy Pistol"] = {
+			MinPlaytime = 14;
+			TeamSpecific = false;
+		};
+		["Sawed Off"] = {
+			MinPlaytime = 12;
+			TeamSpecific = false;
+		};
+		["Riot PDW"] = {
+			MinPlaytime = 24;
+			TeamSpecific = false;
+		};
+		Groza = {
+			MinPlaytime = 30;
+			TeamSpecific = false;
+		};
+		["Service Rifle"] = {
+			MinPlaytime = 36;
+			TeamSpecific = false;
+		};
+	};
+	["Car Dealer"] = {
+		["Wooden Sled"] = {
+			MinPlaytime = 10;
+			TeamSpecific = false;
+		};
+		Convertible = {
+			MinPlaytime = 8;
+			TeamSpecific = false;
+		};
+		Lowrider = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["Lowrider Coupe"] = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["Small Trailer"] = {
+			MinPlaytime = 3;
+			TeamSpecific = false;
+		};
+		["Muscle Coupe"] = {
+			MinPlaytime = 12;
+			TeamSpecific = false;
+		};
+		Fastback = {
+			MinPlaytime = 13;
+			TeamSpecific = false;
+		};
+		["Stainless Sports Car"] = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["Sports Car"] = {
+			MinPlaytime = 18;
+			TeamSpecific = false;
+		};
+		Roadster = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["SUV (Dune)"] = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["SUV (Euro)"] = {
+			MinPlaytime = 6;
+			TeamSpecific = false;
+		};
+		["Family SUV"] = {
+			MinPlaytime = 7;
+			TeamSpecific = false;
+		};
+		Campervan = {
+			MinPlaytime = 12;
+			TeamSpecific = false;
+		};
+	};
+	Trucker = {
+		["Flatnose Truck"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Semi Truck"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+		["Semi Truck (Sleeper)"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+	};
+	["Tow Trucker"] = {
+		["Wrecker Truck"] = {
+			MinPlaytime = 10;
+			TeamSpecific = true;
+		};
+	};
+	["Advanced Car Dealer"] = {
+		["Classic RV"] = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		RV = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["Christmas RV"] = {
+			MinPlaytime = 10;
+			TeamSpecific = false;
+		};
+		Hypercar = {
+			MinPlaytime = 8;
+			TeamSpecific = false;
+		};
+		["Hypercar (Facelift)"] = {
+			MinPlaytime = 10;
+			TeamSpecific = false;
+		};
+		["Humvee (Civilian)"] = {
+			MinPlaytime = 40;
+			TeamSpecific = false;
+		};
+		["Luxury Off-Roader"] = {
+			MinPlaytime = 50;
+			TeamSpecific = false;
+		};
+		Humvee = {
+			MinPlaytime = 60;
+			TeamSpecific = false;
+		};
+		["Snowplow Humvee"] = {
+			MinPlaytime = 72;
+			TeamSpecific = false;
+		};
+	};
+	Criminal = {
+		Drill = {
+			MinPlaytime = 4;
+			TeamSpecific = false;
+		};
+		["Hotwired Printer"] = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		C4 = {
+			MinPlaytime = 6;
+			TeamSpecific = false;
+		};
+	};
+	Mayor = {
+		["Door Ram"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+	};
+	["Crew Leader"] = {
+		["Tommy Gun"] = {
+			MinPlaytime = 0;
+			TeamSpecific = true;
+		};
+	};
+	Crafter = {
+		C4 = {
+			MinPlaytime = 5;
+			TeamSpecific = false;
+		};
+		["Triple Firework Kit"] = {
+			MinPlaytime = 10;
+			TeamSpecific = false;
+		};
+	};
+	Firefighter = {
+		["Fire Engine"] = {
+			MinPlaytime = 5;
+			TeamSpecific = true;
+		};
+		["Tanker Truck"] = {
+			MinPlaytime = 8;
+			TeamSpecific = true;
+		};
+		["Ladder Truck"] = {
+			MinPlaytime = 50;
+			TeamSpecific = true;
+		};
+	};
+}
